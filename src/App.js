a38151d8688a7a8a046125a9569004edbf4d301e
Brian Li <brnli7@gmail.com>
2022-12-01 17:01:24 -0500

    Update copy

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ function App() {
     <div style={{ marginLeft: "8px", maxWidth: "800px" }}>
       <h1>Li Consulting</h1>
       <p>
-        Li Consulting has extensive experience building software solutions for
-        blockchain and cryptocurrencies. These solutions include zkSNARKs, DeFi
-        (EVM and Solana), NFTs, validator operations, and smart contract
-        auditing.
+        Li Consulting is a software consulting company with experience in
+        building full-stack software solutions. These solutions include
+        zkSNARKs, DeFi (EVM and Solana), NFTs, MEV, validator operations and
+        smart contract auditing.
       </p>
-      <p>Contact: brian [at] liconsulting.xyz</p>
+      <p>Inquiries: brian[at]liconsulting.xyz</p>
     </div>
   );
 }
